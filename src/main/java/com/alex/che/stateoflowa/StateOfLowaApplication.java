4bab1d3f1272135f928db2ef4de28c67be391ba5
alexander.chaplygin <external.alexander.chaplygin@Kuehne-Nagel.com>
2019-10-02 10:50:02 +0300

    First commit

---
+++ src/main/java/com/alex/che/stateoflowa/StateOfLowaApplication.java
@@ -0,0 +1,13 @@
+package com.alex.che.stateoflowa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StateOfLowaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StateOfLowaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alex/che/stateoflowa/StateOfLowaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.alex.che.stateoflowa;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StateOfLowaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
